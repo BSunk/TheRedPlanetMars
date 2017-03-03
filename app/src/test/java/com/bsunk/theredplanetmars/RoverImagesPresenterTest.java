13b8344cad710d6190e3d977c9af300cd8057138
Bharat <bharateffect@gmail.com>
2017-03-02 22:45:07 -0800

    Add unit tests for RoverImagesPresenter

--- app/src/test/java/com/bsunk/theredplanetmars/RoverImagesPresenterTest.java
+++ app/src/test/java/com/bsunk/theredplanetmars/RoverImagesPresenterTest.java
@@ -3,10 +3,18 @@ package com.bsunk.theredplanetmars;
 import com.bsunk.theredplanetmars.roverimages.RoverImagesContract;
 import com.bsunk.theredplanetmars.roverimages.RoverImagesPresenter;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import rx.Scheduler;
+import rx.android.plugins.RxAndroidPlugins;
+import rx.android.plugins.RxAndroidSchedulersHook;
+import rx.schedulers.Schedulers;
+
 import static org.mockito.Mockito.verify;
 
 /**
@@ -20,15 +28,25 @@ public class RoverImagesPresenterTest {
 
     private RoverImagesPresenter roverImagesPresenter;
 
+    @BeforeClass
+    public static void setupClass() {
+        RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
+            @Override public Scheduler getMainThreadScheduler() {
+                return Schedulers.immediate();
+            }
+        });
+    }
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         roverImagesPresenter = new RoverImagesPresenter(view);
+
     }
 
     @Test
     public void shouldShowEmptyOnLoadError() {
-        roverImagesPresenter.loadImages(false, 0, 2016, 10, 15);
+        roverImagesPresenter.loadImages(false, 0, 2016, 10, 16);
         verify(view).showListEmpty(true);
     }
 
@@ -36,5 +54,10 @@ public class RoverImagesPresenterTest {
     public void shouldPassImagesOnLoadSuccess() {
         roverImagesPresenter.loadImages(false, 0, 2016, 10, 17);
         verify(view).showList();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        RxAndroidPlugins.getInstance().reset();
     }
 }
